9ceed5faaafaaaa61f7368642e045047be326bab
Garrett Dougherty <57112206+GarrettDougherty@users.noreply.github.com>
2022-01-14 19:18:26 -0500

    Update App.java

--- Hackathon/src/App.java
+++ Hackathon/src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Michael Vanausdall");
-        System.out.println("Michael is epic");
+        System.out.println("Garrett is epic");
     }
 }
