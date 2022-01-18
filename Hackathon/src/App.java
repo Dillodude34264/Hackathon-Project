f55c386c088cdf8ef1b5bd1ce481ab66629a697a
conneben000 <benjamin_connelly@hotmail.com>
2022-01-18 13:28:15 -0500

    Updates

--- Hackathon/src/App.java
+++ Hackathon/src/App.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
 import java.io.Scanner;
 
+////// Garrett can you make mock-ups or a plan for a potential UI - Michael ////////////
+
 public class App {
     Scanner getInput = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        
+
         String input = "";
+        // Continues to ask user what they want to do until the choose student or teacher mode
         while(input.equals("")){
+            // Gets input from user
             System.out.println("Choose student or teacher mode(s/t): ");
             input = getInput.nextLine();
             if(input == "s"){
@@ -14,6 +18,7 @@ public class App {
             }else if(input == "t"){
                 teacherMode();
             }else{
+                // Invalid statement
                 System.out.println("Invalid input. Please try again.");
                 input = "";
 
--- Hackathon/src/Grades.java
+++ Hackathon/src/Grades.java
@@ -5,18 +5,21 @@ public class Grades(){
     private int mean;
     private double stanDev;
 
+    // Constructor to get ArrayList of grades
     public void Grades(ArrayList<Assignment> gP){
         gradePercs = gP;
     }
 
+    // Finds mean of grades from the inputted grades
     public static void findMean(ArrayList<Assignment> gradePercs){
         int sum = 0;
         int numGrades = gradePercs.size();
+        // Loops through grades, adds to sum
         for(int i = 0; i < numGrades; i++){
             int grade = gradePercs.get(i).getScore();
             sum += grade;
         }
-        mean = (int)((sum/numGrades)+0.5);
+        mean = (int)((sum/numGrades)+0.5);  // Divides sum by number of grades
     }
 
 }
