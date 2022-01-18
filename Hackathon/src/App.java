75207bb8e439aec31f191db0a53019bbb25b88fd
conneben000 <benjamin_connelly@hotmail.com>
2022-01-18 12:25:31 -0500

    Adding to idea

--- Hackathon/src/App.java
+++ Hackathon/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
         int numIdeas = 1;
         if (numIdeas == 0) System.out.println("We need more ideas!");
     }
