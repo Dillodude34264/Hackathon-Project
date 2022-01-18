821cfee18dd7a78e5bca386dee630a0d5d867f2f
conneben000 <benjamin_connelly@hotmail.com>
2022-01-18 13:01:47 -0500

    Update Grades.java

--- Hackathon/src/Grades.java
+++ Hackathon/src/Grades.java
@@ -1,5 +1,14 @@
 
 public class Grades(){
-    private ArrayList<Integer> gradePerc = new ArrayList<Integer>();
+    private ArrayList<Assignment> gradePercs = new ArrayList<Assignment>();
+    private int mean;
+    private double stanDev;
+
+    public static findMean(ArrayList<Assignment> gradePercs){
+        int sum = 0;
+        for(int i = 0; i < gradePercs.size(); i++){
+            
+        }
+    }
 
 }
