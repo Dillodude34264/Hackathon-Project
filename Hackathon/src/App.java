bb6887a050b330a8fea9bcd41fb6e760fd08a357
Dillodude34264 <dillodude34264@gmail.com>
2022-01-18 12:15:46 -0500

    Update App.java

--- Hackathon/src/App.java
+++ Hackathon/src/App.java
@@ -1,13 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Michael Vanausdall");
-        System.out.println("Test - Ben");
-        System.out.println("Garrett Dougherty");
-        System.out.println("test3");
-        System.out.println("Garrett is epic");
         int numIdeas = 1;
-
         if (numIdeas == 0) System.out.println("We need more ideas!");
     }
 }
