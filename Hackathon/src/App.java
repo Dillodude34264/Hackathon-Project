6cdd0577731d117674e6e307456d509ff52b405f
conneben000 <benjamin_connelly@hotmail.com>
2022-01-18 13:22:30 -0500

    Update App.java

--- Hackathon/src/App.java
+++ Hackathon/src/App.java
@@ -2,16 +2,30 @@ import java.util.ArrayList;
 import java.io.Scanner;
 
 public class App {
+    Scanner getInput = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        Scanner getInput = new Scanner(System.in);
+        String input = "";
+        while(input.equals("")){
+            System.out.println("Choose student or teacher mode(s/t): ");
+            input = getInput.nextLine();
+            if(input == "s"){
+                studentMode();
+            }else if(input == "t"){
+                teacherMode();
+            }else{
+                System.out.println("Invalid input. Please try again.");
+                input = "";
 
-        int numIdeas = 1;
-        if (numIdeas == 0) System.out.println("We need more ideas!");
+            }
+        }
 
+    }
+    public static void studentMode(){
 
-
-        // Teacher Mode - try different curves
-        
-
+    }
+    public static void teacherMode(){
+        System.out.println("Enter the grade percentages of students on the test as integer numbers; enter -1 to end");
+        int grade = 0;
+        while()
     }
 }
