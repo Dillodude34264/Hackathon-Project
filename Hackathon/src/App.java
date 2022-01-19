239ef18540524582882a8cb6ad7d854b86bfd33a
Dillodude34264 <dillodude34264@gmail.com>
2022-01-19 13:23:33 -0500

    updates

--- Hackathon/src/App.java
+++ Hackathon/src/App.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 
 public class App {
     static ArrayList<Course> courses = new ArrayList<Course>();
+    static Scanner getInput = new Scanner(System.in);
     
     public static void main(String[] args) throws Exception {
-        Scanner getInput = new Scanner(System.in);
         String input = "";
 
         // Continues to ask user what they want to do until the choose student or teacher mode
-        while(input.equals("")){
+        while(true){
             // Gets input from user
             System.out.println("Choose student or teacher mode(s/t) or 'q' to quit: ");
             input = getInput.nextLine();
@@ -30,18 +30,59 @@ public class App {
         }
 
     }
-    public static void studentMode(){
-        Scanner getInput = new Scanner(System.in);
-        String input = "";
 
-        if (courses.size() != 0) {
-            System.out.println("Would you like to view an existing course(1) or create a new one(2)?");
-            input = getInput.nextLine();
+    //Student mode driver
+    public static void studentMode(){
+        String input = "";
+        int selectedCourse;
+        while (true) {
+            //Ask what student wants to do
+            if (courses.size() != 0) {
+                System.out.println("Enter 'v' to view an existing course, 'c' to create a new course, or 'q' to quit:");
+                input = getInput.nextLine().toLowerCase();
+            } else {
+                input = "c";
+            }
+
+            if (input.equals("v")) { //If student wanted to select a previously created course
+                System.out.println("Courses: ");
+                for (int i = 0; i < courses.size(); i++) {
+                    System.out.println("#" + i + " Name: " + courses.get(i).courseName + " Grade: " + courses.get(i).getScoreString() + "%");
+                }
+
+                System.out.println("Enter the # of the desired course or '-1' to cancel ");
+                selectedCourse = getInput.nextInt();
+
+                if (selectedCourse >=0 && selectedCourse < courses.size()) {}
+            }
+
+            else if (input.equals("c")) {
+                String tempName;
+                int tempTotalPoints;
+                int tempStudentPoints;
+
+                System.out.println("Please Enter a Name: ");
+                tempName = getInput.nextLine();
+
+                System.out.println("Enter 'c' to continue to enter grades or 's' to skip and create empty course or hit 'q' to quit: ");
+                input = getInput.nextLine().toLowerCase();
+
+                if (input.equals("c")) {
+
+                } else if (input.equals("s")) {
+                    courses.add(new Course(tempName));
+                }
+            }
+            
+            else { //If the User entered anything other than 'c' or 'v' it quits
+                break;
+            }
         }
     }
 
+    //Teacher mode driver
     public static void teacherMode(){
-        System.out.println("Enter the grade percentages of students on the test as integer numbers; enter -1 to end");
+        System.out.println("Enter the grade percentages of students on the test as integer numbers; enter -1 to quit");
         int grade = 0;
         //while()
     }
--- Hackathon/src/Course.java
+++ Hackathon/src/Course.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 
 public class Course {
-    private String courseName; //Name of the course
-    private int basePoints; //# of points at the time the Course was created
-    private int totalPoints; //Total # of points calculated to include added Assignments
-    private int studentPoints; //Total # of points the student has calculated to include added 
-    private ArrayList<Assignment> courseAssignments = new ArrayList<Assignment>();
-    private ArrayList<Category> courseCategories = new ArrayList<Category>();
+    public String courseName; //Name of the course
+    public int basePoints; //# of points at the time the Course was created
+    public int totalPoints; //Total # of points calculated to include added Assignments
+    public int studentPoints; //Total # of points the student has calculated to include added 
+    public ArrayList<Assignment> courseAssignments = new ArrayList<Assignment>();
+    public ArrayList<Category> courseCategories = new ArrayList<Category>();
 
     public Course(String name, int points, int score) { //Takes the name of the course, the total # of points at the time of creation, and the percent the student has in the course
         courseName = name;
@@ -16,5 +16,15 @@ public class Course {
 
     public Course(String name) { //Constructor for when the student is making a new course that they have no points in yet
         courseName = name;
+        basePoints = 0;
+        studentPoints = 0;
+    }
+
+    public String getScoreString() {
+        if (basePoints + totalPoints != 0) {
+            return "" + (studentPoints/(basePoints + totalPoints));
+        } else {
+            return "N/A";
+        }
     }
 }
